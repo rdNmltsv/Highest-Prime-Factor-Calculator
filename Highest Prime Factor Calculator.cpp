941a3ae970b91a88e0f23b5c635587e77eb787c1
malrv <malrv@tavrida.com>
2022-08-24 11:36:26 +0300

    Implements next functionality:
    - console smart input/output
    - trial division factorization algorithm
    - calculate and display elapsed time for factorization in milliseconds

--- Highest Prime Factor Calculator.cpp
+++ Highest Prime Factor Calculator.cpp
@@ -1,20 +1,74 @@
-﻿// Highest Prime Factor Calculator.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
+﻿#include <iostream>
+#include <iomanip>
+#include <vector>
+#include <cmath>
+#include <algorithm>
+#include <functional>
+#include <limits>
+#include <chrono>
 
-#include <iostream>
+template<typename T>
+void printVector(const std::vector<T>& data) {
+    std::copy(data.begin(), data.end(), std::ostream_iterator<T>(std::cout, " "));
+}
+
+template<typename T>
+T getMaxElem(std::vector<T> &&factors) {
+    return *std::max_element(factors.begin(), factors.end());
+}
+
+/* Trial division integer factorization algorithm */
+template<typename T>
+std::vector<T> trialDivision(T n) {
+    std::vector<T> factors{};
+    for (T i = 2; i <= std::sqrt(n); i++) {
+        while (n % i == 0) {
+            factors.emplace_back(i);
+            n /= i;
+        }
+    }
+
+    if (n != 1)
+        factors.emplace_back(n);
+
+    return factors;
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    std::cout << "<< Highest Prime Factor Calculator >>";
+
+    for (;;) {
+
+        std::cout << "\nEnter positive real number (EOF to exit): ";
+        long long n{};
+        std::cin >> n;
+
+        if (std::cin.eof())
+            break;
+
+        if (std::cin.fail() || n < 0) {
+            std::cout << "You must enter positive real number.";
+        }
+        else {        
+            auto startTime = std::chrono::high_resolution_clock::now();
+
+            auto factorFunc = trialDivision<decltype(n)>;
+
+            auto prime = getMaxElem(std::move(factorFunc(n)));
+
+            auto endTime = std::chrono::high_resolution_clock::now();
+            std::chrono::duration<double, std::milli> elapsedTime_ms = endTime - startTime;
+
+            std::cout << "Highest prime factor of " << n << " is " << prime << '.';
+          
+            std::cout << "\nElapsed time is " << std::fixed << std::setprecision(3) << elapsedTime_ms.count() << " ms.";
+        }
+
+        std::cin.clear();
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    }
+
+    std::cout << "\nExiting..." << std::endl;
+    return EXIT_SUCCESS;
 }
-
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
